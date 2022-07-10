4a72c0b6ddc1bf682283e17630adf5b65aee0b2a
Sushmi-3 <101321632+Sushmi-3@users.noreply.github.com>
2022-07-10 10:18:53 -0700

    Add files via upload

--- Google Keep/javascript.js
+++ Google Keep/javascript.js
@@ -1,15 +1,17 @@
-let addBtn = document.getElementById("add-button");
+let addBtn = document.getElementById("add-btn");
 addBtn.addEventListener("click", function(e) {
 
-  let addTitle = document.getElementById("text");
+  let addTitle = document.getElementById("note-title");
   let addTxt = document.getElementById("note-text");
   
+    // if (addTitle.value == "" || addTxt.value == "") {
+    //     return alert("Please add Note Title and Details")
+    // }
 
   let notes = localStorage.getItem("notes");
   if (notes == null) {
     notesObj = [];
   } else {
-    
     notesObj = JSON.parse(notes);
   }
   let myObj = {
@@ -23,24 +25,50 @@ addBtn.addEventListener("click", function(e) {
 //   console.log(notesObj);
   showNotes();
 });
-function showNotes(){
-  let notes=localStorage.getItem("notes");
-  if(notes==null){
-    notesObj=[];
+
+// Function to show elements from localStorage
+function showNotes() {
+  let notes = localStorage.getItem("notes");
+  if (notes == null) {
+    notesObj = [];
+  } else {
+    notesObj = JSON.parse(notes);
   }
-  else{
-    notesObj=Json.stringyfiy(notes);
-  }
-  let display="";
-  notesObj.forEach(function(element,index){
-    // display +=`
-    // <div class="note">
-    // <p class="note-counter">""</p>
-    // <h3>Todays date</h3>
-    // <p>2nd of july 2022</p>
-    // <button id="delete">Delete</button>
-    // <button id="edit">Edit</button>
-  `;
-  })
+  let html = "";
+  notesObj.forEach(function(element, index) {
+    html += `
+        <div class="note">
+          
+            <h3 class="note-title"> ${element.title} </h3>
+            <p class="note-text"> ${element.text}</p>
+            <button id="${index}"onclick="deleteNote(this.id)" class="note-btn">Delete Note</button>
+            <button id="${index}"onclick="editNote(this.id)" class="note-btn edit-btn">Edit Note</button>
+        </div>
+            `;
+  });
+  let notesElm = document.getElementById("notes");
+  if (notesObj.length != 0) {
+    notesElm.innerHTML = html;
+  } else {
+    notesElm.innerHTML = `No Notes Yet! Add a note using the form above.`;
   }
 }
+
+// Function to delete a note
+function deleteNote(index) {
+    let confirmDel = confirm();
+    if (confirmDel == true) {
+        let notes = localStorage.getItem("notes");
+        if (notes == null) {
+            notesObj = [];
+        } else {
+            notesObj = JSON.parse(notes);
+        }
+
+        notesObj.splice(index, 1);
+        localStorage.setItem("notes", JSON.stringify(notesObj));
+        showNotes();
+    }
+  
+}
+showNotes();
