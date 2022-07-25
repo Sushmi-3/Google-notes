afe62f079b6e85c66dc4096b3ae9aa51854c0a39
Sushmi-3 <101321632+Sushmi-3@users.noreply.github.com>
2022-07-25 03:33:21 -0700

    Add files via upload

--- Google Keep/javascript.js
+++ Google Keep/javascript.js
@@ -1,123 +1,113 @@
-function red(){
-  document.getElementById('text').style.background ="LightCoral";
-  // let isUpdate=true;
-}
+const noteContainer = document.querySelector('.note-container');
+const modalContainer = document.querySelector('.modal-container');
+const form = document.querySelector('form');
+const titleInput = document.querySelector('#title');
 
-function blue(){
-  document.getElementById('text').style.background ="LightCoral";
-  // let isUpdate=false;
-}
-
-const addBox =document.querySelector(".add-box"),
-popupBox = document.querySelector(".popup-box");
-
-popupTitle = popupBox.querySelector("header p");
-closeIcon = popupBox.querySelector("header i");
-titleTag = popupBox.querySelector("input"),
-desTag = popupBox.querySelector(".des"),
-addBtn = popupBox.querySelector("button");
-
-const notes =JSON.parse(localStorage.getItem("notes")||"[]"); 
-
-addBox.addEventListener("click",()=>{
- 
-  popupBox.classList.add("show");
-});
-
-addBtn.addEventListener("click",v=>{
-  v.preventDefault();
-  let noteTitle = titleTag.value,
-  noteDesc =desTag.value;
-  if(noteTitle || noteDesc){
-      
-      let noteInfo ={
-          title:noteTitle,description:noteDesc
-      }
-      if(!isUpdate){
-          notes.push(noteInfo);
-      }
-      else{
-          isUpdate=false;
-          notes[updateId]=noteInfo;
-      }
-      
-      notes.push(noteInfo);//adding new note to notes
-      //saving notes to localstorage
-      localStorage.setItem("notes",JSON.stringify(notes));
-      closeIcon.click();
-      showNotes();
+// Class: for creating a  new  note
+class Note {
+  constructor(title, body) {
+    this.title = title;
+    this.body = body;
+    this.id = Math.random();
   }
-      
+}
+
+let addBtn = document.getElementById("add-btn");
+addBtn.addEventListener("click", function(e) {
+
+  let addTitle = document.getElementById("note-title");
+  let addTxt = document.getElementById("note-text");
   
-})
-
-
-let isUpdate = false,updateId;
-function updateNote(noteId,title,desc){
-  isUpdate =true;
-  updateId=noteId; 
-  addBox.click();
-  titleTag.value =title;
-  desTag.value =desc;
-  addBtn.innerText ="update note";
-  // popupTitle.innerText ="update a note";
-  console.log(noteId,title,desc);
-}
-
-
-function deleteNote(noteId){
-  notes.splice(noteId,1);
-  localStorage.setItem("notes",JSON.stringify(notes));
-  showNotes();
-}
-function showNotes(){
-  document.querySelectorAll(".note").forEach(note=>note.remove());
-  notes.forEach((note,index)=>{
-      let liTag=`<li class="note">
-      <div class="details">
-        <p>${note.title}</p>
-        <span>${note.description}</span>
-      </div>
-      <div class="bottom-content">
-       
-        <div class="settings">
-          <i onclick="showMenu(this)"class="uil uil-ellipsis-h"></i>
-          <ul class="menu">
-            <li onclick ="updateNote(${index},'${note.title}','${note.description}')">
-            <i class="uil uil-pen"></i>edit</li>
-            <li onclick ="deleteNote(${index})"><i class="uil uil-trash"></i>delete</li>
-          </ul>
-        </div>
-      </div>
-    </li>`
-    addBox.insertAdjacentHTML("afterend",liTag);
-  });
-}
-showNotes();
-
-function showMenu(choose){
-  choose.parentElement.classList.add("show");
-  document.addEventListener("click",e =>{
-      if(e.target.tagName !="I" || e.target !=choose){
-          choose.parentElement.classList.remove("show");
-      }
-  });
-}
-
-closeIcon.addEventListener("click",()=>{
-  ISUpdate =false;
-  titleTag.value ="";
-  desTag.value ="";
- 
-  popupBox.classList.remove("show");
+  let notes = localStorage.getItem("notes");
+  if (notes == null) {
+    notesObj = [];
+  } else {
+    notesObj = JSON.parse(notes);
+  }
+  let myObj = {
+    title: addTitle.value,
+    text: addTxt.value
+  }
+  notesObj.push(myObj);
+  localStorage.setItem("notes", JSON.stringify(notesObj));
+  addTxt.value = "";
+  addTitle.value = "";
+//   console.log(notesObj);
+displayNotes();
 });
 
-function updateNote(noteId,title,desc){
-  isUpdate =true;
-  updateId=noteId; 
-  addBox.click();
-  titleTag.value =title;
-  desTag.value =desc;
-
-  console.log(noteId,title,desc);
+// Function to show elements from localStorage
+function displayNotes() {
+  let notes = localStorage.getItem("notes");
+  if (notes == null) {
+    notesObj = [];
+  } else {
+    notesObj = JSON.parse(notes);
+  }
+  let html = "";
+  notesObj.forEach(function(element, index) {
+    html += `
+        <div class="note">
+            <p class="note-counter"></p>
+            <h3 class="note-title"> ${element.title} </h3>
+            <p class="note-text"> ${element.text}</p>
+            <button id="${index}"onclick="deleteNote(this.id)" class="note-btn">Delete</button>
+            <button id="${index}"onclick="editNote(this.id)" class="note-btn edit-btn">Edit</button>
+        </div>
+            `;
+  });
+  let notesElm = document.getElementById("notes");
+  if (notesObj.length != 0) {
+    notesElm.innerHTML = html;
+  } else {
+    notesElm.innerHTML = `No Notes Yet! Add a note using the form above.`;
+  }
 }
+
+// Function to delete a note
+function deleteNote(index) {
+//   console.log("I am deleting", index);
+    let confirmDel = confirm("Delete this note?");
+    if (confirmDel == true) {
+        let notes = localStorage.getItem("notes");
+        if (notes == null) {
+            notesObj = [];
+        } else {
+            notesObj = JSON.parse(notes);
+        }
+
+        notesObj.splice(index, 1);
+        localStorage.setItem("notes", JSON.stringify(notesObj));
+        displayNotes();
+    }
+  
+}
+
+// Function to Edit the Note
+function editNote(index) {
+    let notes = localStorage.getItem("notes");
+    let addTitle = document.getElementById("note-title");
+    let addTxt = document.getElementById("note-text");
+
+    if (addTitle.value !== "" || addTxt.value !== "") {
+      return alert("Please clear the form before editing a note")
+    } 
+
+    if (notes == null) {
+      notesObj = [];
+    } else {
+      notesObj = JSON.parse(notes);
+    }
+    console.log(notesObj);
+
+    notesObj.findIndex((element, index) => {
+      addTitle.value = element.title;
+      addTxt.value = element.text;
+    })
+    notesObj.splice(index, 1);
+        localStorage.setItem("notes", JSON.stringify(notesObj));
+        displayNotes();
+}
+
+
+displayNotes();
